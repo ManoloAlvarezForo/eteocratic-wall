ecdb3dd4cf3ca1a8563ea753ea7b4bed4ed3f2f7
Manolo Alvarez Foro <af.manolo@gmail.com>
2018-02-25 17:30:28 -0400

    Added praching basic component

---
+++ app/components/Preaching.js
@@ -0,0 +1,23 @@
+// @flow
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Home.css';
+// Material-UI
+import RaisedButton from 'material-ui/RaisedButton'
+import TextField from 'material-ui/TextField'
+import AppBar from 'material-ui/AppBar';
+import MoreVert from 'material-ui/svg-icons/navigation/more-vert';
+import IconButton from 'material-ui/IconButton';
+
+
+export default class Preaching extends Component<Props> {
+  props: Props;
+
+  render() {
+    return (
+      <div>
+          Preaching
+      </div>
+    );
+  }
+}
